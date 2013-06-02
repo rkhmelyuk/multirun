c3bb60260ce259cd5df8e3fc9a85b893fe7e97d1
Ruslan Khmelyuk <ruslan.khmelyuk@gmail.com>
2013-06-02 14:53:38 -0700

    Small cleanup for the test project.

--- test/test-prj/src/com/khmelyuk/multirun/testprj/App.java
+++ test/test-prj/src/com/khmelyuk/multirun/testprj/App.java
@@ -1,8 +1,8 @@
 package com.khmelyuk.multirun.testprj;
 
-public class AppOne {
+public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello from app1");
+        System.out.println("Hello from " + args[0]);
     }
 }
--- test/test-prj/src/com/khmelyuk/multirun/testprj/AppThree.java
+++
@@ -1,8 +0,0 @@
-package com.khmelyuk.multirun.testprj;
-
-public class AppThree {
-
-    public static void main(String[] args) {
-        System.out.println("Hello from app3");
-    }
-}
