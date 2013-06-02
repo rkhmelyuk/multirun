fb9e9f4985ac7c178c0f6340d7d5e3cd9d9ee830
Ruslan Khmelyuk <ruslan.khmelyuk@gmail.com>
2013-06-02 16:28:16 -0700

    Better icon for the run configuration type.

--- main/src/java/com/khmelyuk/multirun/MultirunConfigurationType.java
+++ main/src/java/com/khmelyuk/multirun/MultirunConfigurationType.java
@@ -3,11 +3,12 @@ package com.khmelyuk.multirun;
 import com.intellij.execution.configurations.ConfigurationFactory;
 import com.intellij.execution.configurations.ConfigurationTypeBase;
 import com.intellij.execution.configurations.RunConfiguration;
+import com.intellij.icons.AllIcons;
 import com.intellij.openapi.project.Project;
 
 public class MultirunConfigurationType extends ConfigurationTypeBase {
     public MultirunConfigurationType() {
-        super("Multirun", "Multirun", "Run multiple configurations", ConfigurationFactory.ADD_ICON);
+        super("Multirun", "Multirun", "Run multiple configurations", AllIcons.Actions.Rerun);
         addFactory(new ConfigurationFactory(this) {
             @Override
             public RunConfiguration createTemplateConfiguration(Project project) {
