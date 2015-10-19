6af79b157fadfabc89c52b9a584293bdb3f8d7a7
Ruslan Khmelyuk <ruslan.khmelyuk@gmail.com>
2015-10-18 17:51:29 -0700

    Fix debug and use JRebel for idea14.

--- src/main/java/com/khmelyuk/multirun/MultirunRunner.java
+++ src/main/java/com/khmelyuk/multirun/MultirunRunner.java
@@ -13,6 +13,9 @@ import org.jetbrains.annotations.NotNull;
  */
 public class MultirunRunner extends DefaultProgramRunner {
 
+    public static String JREBEL_EXECUTOR_ID = "JRebel Executor";
+    public static String JREBEL_DEBUG_ID = "JRebel Debug";
+
     @NotNull
     @Override
     public String getRunnerId() {
@@ -22,7 +25,9 @@ public class MultirunRunner extends DefaultProgramRunner {
     @Override
     public boolean canRun(@NotNull String executorId, @NotNull RunProfile runProfile) {
         return runProfile instanceof MultirunRunConfiguration &&
-                (DefaultRunExecutor.EXECUTOR_ID.equals(executorId)
-                        || DefaultDebugExecutor.EXECUTOR_ID.equals(executorId));
+                (DefaultRunExecutor.EXECUTOR_ID.equalsIgnoreCase(executorId)
+                        || DefaultDebugExecutor.EXECUTOR_ID.equalsIgnoreCase(executorId)
+                        || JREBEL_EXECUTOR_ID.equalsIgnoreCase(executorId)
+                        || JREBEL_DEBUG_ID.equalsIgnoreCase(executorId));
     }
 }
